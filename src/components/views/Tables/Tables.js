b2204f375f939f6c6aa1c6a285f34669fbd6cfae
Justyna Szewczyk <justyna0829@gmail.com>
2021-04-27 18:23:03 +0200

    Start with Material UI

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -5,10 +5,10 @@ import {Link} from 'react-router-dom';
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Tables Booking ID</Link>
     <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>Tables Booking New</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`}>Tables Events ID</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Tables Booking ID</Link>
     <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>Tables Events New</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`}>Tables Events ID</Link>
   </div>
 );
 

2b7c341a7fabb350c866a3a128828991adb0e6b3
Justyna Szewczyk <justyna0829@gmail.com>
2021-04-29 14:35:57 +0200

    Start with Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Login = () => (
-  <div className={styles.component}>
-    <h2>Login view</h2>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
-export default Login;
+export default function Login () {
+  const classes = useStyles();
+
+  return (
+    <div className={styles.component}>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Login"
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
